57efe2d79ecbffa28c04636619239892a9281522
bats31 <tobias_schlauch@gmx.de>
2021-07-24 18:12:04 +0200

    scenarionumber: 5

--- webapp.js
+++ webapp.js
@@ -10,7 +10,7 @@ http.createServer(function (req, res) {
   // HTTP response header - the content will be HTML MIME type
   res.writeHead(200, { 'Content-Type': "text/html" });
   // Write out the HTTP response body
-  res.write("@@TEST@@");
+  res.write("@@STR@@");
  
   // End of HTTP response
   res.end();
